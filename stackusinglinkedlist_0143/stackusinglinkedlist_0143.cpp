0e71cd9334d90fd5b83e87c686af6441f52d8d73
RMadya <kereyoben75@gmail.com>
2024-05-10 12:25:05 +0700

    kasus 3 mendaptkan nilai dari top element

--- stackusinglinkedlist_0143/stackusinglinkedlist_0143.cpp
+++ stackusinglinkedlist_0143/stackusinglinkedlist_0143.cpp
@@ -87,4 +87,13 @@ int main() {
 			}
 			break;
 
+		case 3:
+			if (!stack.isEmpty()) {
+				stack.peek();
+			}
+			else {
+				cout << "Stack is empty. No top value." << endl;
+			}
+			break;
+
 		
